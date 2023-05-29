a0a1638370c19b92e3f233d2bcbcb1ce4116f6f5
Siham EL KHOUCHANI <sihamelkhouchani@gmail.com>
2023-05-29 15:16:30 +0100

    Adding reference to /

--- src/main/java/com/example/booktopia/controllers/AuthorController.java
+++ src/main/java/com/example/booktopia/controllers/AuthorController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -102,4 +103,11 @@ public class AuthorController {
         modelMap.addAttribute("currentPage",page);
         return"AuthorsList";
     }
+
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/authorsList";
+    }
+
+
 }
