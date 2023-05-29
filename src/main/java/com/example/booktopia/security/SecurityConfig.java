82739aba71d7d3ddcb4054438d9a69b54978f09c
Siham EL KHOUCHANI <sihamelkhouchani@gmail.com>
2023-05-29 20:23:57 +0100

    Affecting Roles

--- src/main/java/com/example/booktopia/security/SecurityConfig.java
+++ src/main/java/com/example/booktopia/security/SecurityConfig.java
@@ -18,53 +18,71 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.formLogin();
-        httpSecurity.authorizeHttpRequests().requestMatchers("/createAuthor").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/updateAuthor").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/saveAuthor").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/showAuthor").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/deleteAuthor").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/authorsList").hasAnyRole("ADMIN","USER");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/createAuthor").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/updateAuthor").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/saveAuthor").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/showAuthor").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/deleteAuthor").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/authorsList").hasAnyRole("ADMIN","USER");
+//
+//
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/createCategory").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/updateCategory").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/saveCategory").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/showCategory").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/deleteCategory").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/categoriesList").hasAnyRole("ADMIN","USER");
+//
+//
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/createPubHouse").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/updatePubHouse").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/savePubHouse").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/showPubHouse").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/deletePubHouse").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/pubHousesList").hasAnyRole("ADMIN","USER");
+//
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/createBook").hasAnyRole("ADMIN","AUTHOR");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/updateBook").hasAnyRole("ADMIN","AUTHOR");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/saveBook").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/showBook").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/deleteBook").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/booksList").hasAnyRole("ADMIN","USER");
+//
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/createReview").hasRole("USER");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/updateReview").hasAnyRole("USER");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/saveReview").hasRole("USER");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/showReview").hasRole("USER");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/deleteReview").hasRole("USER");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/reviewsList").hasAnyRole("ADMIN","AUTHOR");
+//
+//
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/createUser").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/updateUser").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/saveUser").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/showUser").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/deleteUser").hasRole("ADMIN");
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/usersList").hasRole("ADMIN");
 
 
-        httpSecurity.authorizeHttpRequests().requestMatchers("/createCategory").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/updateCategory").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/saveCategory").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/showCategory").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/deleteCategory").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/categoriesList").hasAnyRole("ADMIN","USER");
+        httpSecurity.authorizeHttpRequests().requestMatchers("/createAuthor","/updateAuthor","/saveAuthor", "/showAuthor", "/deleteAuthor",
+                "/createCategory", "/updateCategory", "/saveCategory", "/showCategory", "/deleteCategory",
+                "/createPubHouse", "/updatePubHouse", "/savePubHouse", "/showPubHouse", "/deletePubHouse",
+                "/createBook", "/updateBook", "/saveBook", "/showBook", "/deleteBook",
+                "/createReview", "/updateReview", "/saveReview", "/showReview", "/deleteReview",
+                "/createUser", "/updateUser", "/saveUser", "/showUser", "/deleteUser", "/usersList").hasRole("ADMIN");
 
 
-        httpSecurity.authorizeHttpRequests().requestMatchers("/createPubHouse").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/updatePubHouse").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/savePubHouse").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/showPubHouse").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/deletePubHouse").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/pubHousesList").hasAnyRole("ADMIN","USER");
-
-        httpSecurity.authorizeHttpRequests().requestMatchers("/createBook").hasAnyRole("ADMIN","AUTHOR");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/updateBook").hasAnyRole("ADMIN","AUTHOR");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/saveBook").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/showBook").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/deleteBook").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/booksList").hasAnyRole("ADMIN","USER");
-
-        httpSecurity.authorizeHttpRequests().requestMatchers("/createReview").hasRole("USER");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/updateReview").hasAnyRole("USER");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/saveReview").hasRole("USER");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/showReview").hasRole("USER");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/deleteReview").hasRole("USER");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/reviewsList").hasAnyRole("ADMIN","AUTHOR");
-
-
-        httpSecurity.authorizeHttpRequests().requestMatchers("/createUser").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/updateUser").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/saveUser").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/showUser").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/deleteUser").hasRole("ADMIN");
-        httpSecurity.authorizeHttpRequests().requestMatchers("/usersList").hasRole("ADMIN");
+        httpSecurity.authorizeHttpRequests().requestMatchers("/authorsList",
+                "/categoriesList",
+                "/pubHousesList",
+                "/booksList",
+                "/reviewsList"
+                ).hasAnyRole("ADMIN","USER","AUTHOR");
 
 
         httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
+
+        httpSecurity.exceptionHandling().accessDeniedPage("/accessDenied");
         return httpSecurity.build();
 
     }
@@ -72,7 +90,7 @@ public class SecurityConfig {
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
         return new  InMemoryUserDetailsManager(
-                User.withUsername("admin").password(passwordEncoder.encode("admin123")).roles("ADMIN","USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("admin123")).roles("ADMIN").build(),
                 User.withUsername("author").password(passwordEncoder.encode("author123")).roles("AUTHOR").build(),
                 User.withUsername("user").password(passwordEncoder.encode("user123")).roles("USER").build()
 
